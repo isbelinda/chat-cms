15683dab3e3d2fa1ffd2c7721170b0c838c3f1c6
Belinda <b.bowlyjung@gmail.com>
2016-12-21 17:15:55 +0700

    fix style

--- script/app.js
+++ script/app.js
@@ -18,67 +18,21 @@ app.controller('chatController',['$scope', '$firebaseArray', function($scope, $f
     };
     
     init();
-    
-    
-//     var URL_API = 'https://fir-webchat-31ebc.firebaseio.com/';
-//     var myFirebase = new Firebase(URL_API);
-//     var sync = $firebase(myFirebase);
-//
-//     $scope.send = function(){
-//         var data = {
-//             username: $scope.data.username,
-//             text: $scope.data.text,
-//             posteddate: Date.now()
-//         };
-//         console.log(data);
-//         myFirebase.push(data);
-//        
-//         $scope.data.text = '';
-//     };
-//
-//     /** Function to add a data listener **/
-//     function startListening(){
-//         myFirebase.on('child_added', function(snapshot) {
-//             var msg = snapshot.val();
-//            
-// //            var msgUsernameElement = document.createElement("b");
-// //            msgUsernameElement.textContent = msg.username;
-// //
-// //            var msgTextElement = document.createElement("p");
-// //            msgTextElement.textContent = msg.text;
-// //
-// //            var msgElement = document.createElement("div");
-// //            msgElement.appendChild(msgUsernameElement);
-// //            msgElement.appendChild(msgTextElement);
-// //
-// //            msgElement.className = "msg";
-// //            document.getElementById("results").appendChild(msgElement);
-//             displayChatMessage(msg.username, msg.text);
-//         });
-//     };
-//
-//     function displayChatMessage(name, text) {
-//         var msgUsernameElement = document.createElement("b");
-//         msgUsernameElement.textContent = name;
-//
-//         var msgTextElement = document.createElement("p");
-//         msgTextElement.textContent = text;
-//
-//         var msgElement = document.createElement("div");
-//         msgElement.appendChild(msgUsernameElement);
-//         msgElement.appendChild(msgTextElement);
-//
-//         msgElement.className = "msg";
-//         document.getElementById("results").appendChild(msgElement);
-// //        document.getElementById("results").append(msgElement);
-// //        $('<div/>').text(text).prepend($('<em/>').text(name + ': ')).appendTo($('#results'));
-// //        $('#results')[0].scrollTop = $('#results')[0].scrollHeight;
-//     };
-//
-//     // Begin listening for data
-//     startListening();
-
 }]);
+
+app.directive('myEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.myEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
 
 app.directive('scrollBottom', function () {
     return {
@@ -89,12 +43,13 @@ app.directive('scrollBottom', function () {
             scope.$watchCollection('scrollBottom', function (newValue) {
                 if (newValue)
                 {
+                    console.log(element[0].scrollTop = element[0].scrollHeight);
                     element[0].scrollTop = element[0].scrollHeight;
                 }
             });
         }
     }
-})
+});
 
 
 
